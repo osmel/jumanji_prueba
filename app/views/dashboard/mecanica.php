cc6b4ec2f64102a09f79b0d19df773bf7e070eec
lenovo-PC\Adrian <guerreroadrian1111@gmail.com>
2017-12-09 19:17:44 -0600

    subiendo cambios maquetas

--- app/views/dashboard/mecanica.php
+++ app/views/dashboard/mecanica.php
@@ -1,10 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <?php $this->load->view( 'header' ); ?>
 <?php $this->load->view( 'navbar' ); ?>
-<style>
-body{background-image: url(../img/fondo2.jpg);
-background-color: #000000;}
-</style>
+
 
  <!-- contenido-->
 <div class="container mecanica">
@@ -17,10 +14,10 @@ background-color: #000000;}
 			<img src="<?php echo base_url()?>img/meca5.png" class="img-responsive img-center">
 		</div>
 		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 text-center mecans1">
-			<img src="<?php echo base_url()?>img/meca1.png" class="img-responsive img-center">
+			<!-- <img src="<?php echo base_url()?>img/meca1.png" class="img-responsive img-center">
 			<img src="<?php echo base_url()?>img/meca7.png" class="img-responsive img-center">
 			<img src="<?php echo base_url()?>img/meca3.png" class="img-responsive img-center">
-			<img src="<?php echo base_url()?>img/meca4.png" class="img-responsive img-center">
+			<img src="<?php echo base_url()?>img/meca4.png" class="img-responsive img-center"> -->
 		</div>
 		<!-- <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
 			<img src="<?php echo base_url().$this->session->userdata('c24'); ?>" class="img-responsive img-center">
